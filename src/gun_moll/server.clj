b0f51b84b7d568eea567f3f24644cad4a03644f8
Daniel Woelfel <dwwoelfel@gmail.com>
2014-10-05 11:52:01 -0700

    make the example linkable

--- src/gun_moll/server.clj
+++ src/gun_moll/server.clj
@@ -53,7 +53,7 @@
         xml/emit-str)))
 
 (defroutes all-routes
-  (GET "/" [] (fn [req] {:status 200 :body "<html><head><title>Gun Moll -- RSS Feeds for your Favorite Hacker News Commenters</title></head><body><p>Gun Moll gives you an RSS feed of Hacker News comments for your favorite HN commenters.</p> <p>Point your rss reader at http://gun-moll.herokuapp.com/threads?id=HN-USERNAME</p> <p>For example, to get an rss feed for patio11: http://gun-moll.herokuapp.com/threads?id=patio11</p></body></html>"}))
+  (GET "/" [] (fn [req] {:status 200 :body "<html><head><title>Gun Moll -- RSS Feeds for your Favorite Hacker News Commenters</title></head><body><p>Gun Moll gives you an RSS feed of Hacker News comments for your favorite HN commenters.</p> <p>Point your rss reader at http://gun-moll.herokuapp.com/threads?id=HN-USERNAME</p> <p>For example, to get an rss feed for patio11: <a href='http://gun-moll.herokuapp.com/threads?id=patio11'>>http://gun-moll.herokuapp.com/threads?id=patio11</a></p></body></html>"}))
   (GET "/ping" [] (fn [req] {:status 200 :body "pong"}))
   (GET "/threads" [id] [] (fn [req]
                             (log/infof "Fetching threads for %s" id)
